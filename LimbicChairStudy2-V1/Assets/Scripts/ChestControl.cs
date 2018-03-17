0d5fa9f06a40f2de613a00401bd4d5af3ef96ff2
Sunny <sunnyzhangyaying@gmail.com>
2018-03-17 12:34:24 -0700

    Initialization
    Put the project in git to sync.

---
+++ LimbicChairStudy2-V1/Assets/Scripts/ChestControl.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestControl : MonoBehaviour
+{
+	public bool found = false;
+	public bool foundTrigger = false;
+	public Collider player;
+	public GameObject chest_open;
+	public GameObject chest_closed;
+	public Transform position1, position2, position3;
+
+	// Use this for initialization
+	void Start ()
+	{
+		transform.position = position1.position;
+		transform.rotation = position1.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+		if ((!found && chest_closed.activeSelf) || (found && chest_open.activeSelf))
+			return;
+		else {
+			ToggleOpeness ();
+		}
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other == player) {
+			found = true;
+			foundTrigger = true;
+		}
+	}
+
+	public void Open ()
+	{
+		chest_open.SetActive (true);
+		chest_closed.SetActive (false);
+	}
+
+	public void Close ()
+	{
+		chest_open.SetActive (false);
+		chest_closed.SetActive (true);
+	}
+
+	public void ToggleOpeness ()
+	{
+		chest_open.SetActive (!chest_open.activeSelf);
+		chest_closed.SetActive (!chest_closed.activeSelf);
+	}
+}
---
+++ LimbicChairStudy2-V1/Assets/Scripts/ProcessManager.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//using UnityEditor;
+using System.IO;
+using UnityEngine.UI;
+
+public class ProcessManager : MonoBehaviour
+{
+	public enum ProcessState
+	{
+		PREPAREATION,
+		SECTION_1,
+		INTERSECTION_1,
+		SECTION_2,
+		INTERSECTION_2,
+		SECTION_3,
+		INTERSECTION_3
+	}
+
+	public ProcessState state = ProcessState.PREPAREATION;
+	float time = 0.0f;
+	public string ParticipantID = "____";
+	public ChestControl chest;
+	public Flying flying;
+
+	// Use this for initialization
+	void Start ()
+	{
+		WriteString ("\nParticipant ID: " + ParticipantID + "\tDate: " + System.DateTime.Now);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Debug.Log ("state: " + state);
+		if (state == ProcessState.SECTION_1) {
+			if (chest.foundTrigger) {
+				WriteString ("time to find the Chest at position 1: " + time + "s.");
+				state = ProcessState.INTERSECTION_1;
+				chest.foundTrigger = false;
+			}
+		} else if (state == ProcessState.SECTION_2) {
+			if (chest.foundTrigger) {
+				WriteString ("time to find the Chest at position 2: " + time + "s.");
+				state = ProcessState.INTERSECTION_2;
+				chest.foundTrigger = false;
+			}
+		} else if (state == ProcessState.SECTION_3) {
+			if (chest.foundTrigger) {
+				WriteString ("time to find the Chest at position 3: " + time + "s.");
+				state = ProcessState.INTERSECTION_3;
+				time = 0;
+				chest.foundTrigger = false;
+			}
+		}
+
+		time += Time.deltaTime;
+
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			time = 0;
+			chest.ToggleOpeness ();
+			chest.transform.position = chest.position1.position;
+			chest.transform.rotation = chest.position1.rotation;
+			state = ProcessState.SECTION_1;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			time = 0;
+			chest.ToggleOpeness ();
+			chest.transform.position = chest.position2.position;
+			chest.transform.rotation = chest.position2.rotation;
+			state = ProcessState.SECTION_2;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			time = 0;
+			chest.ToggleOpeness ();
+			chest.transform.position = chest.position3.position;
+			chest.transform.rotation = chest.position3.rotation;
+			state = ProcessState.SECTION_3;
+		}
+	}
+
+	void OnApplicationQuit ()
+	{		
+		if (!chest.found)
+			WriteString ("Chest not found. Time of the session: " + time + " s.");
+		WriteString ("\n");
+	}
+
+	void WriteString (string s)
+	{
+		string path = "Assets/Data.txt";
+
+		StreamWriter writer = new StreamWriter (path, true);
+		writer.WriteLine (s);
+		writer.Close ();
+	}
+
+}
